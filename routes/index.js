ac428357bec6549e050f42fe3481f748bcbede83
Jared Mezquita <mezquitaj@gmail.com>
2021-06-16 18:52:24 -0400

    added views via EJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Shelly",
+    added: new Date()
+  },
+  {
+    text: "Hello!",
+    user: "Odel",
+    added: new Date()
+
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message board', messages: messages });
 });
 
 module.exports = router;
