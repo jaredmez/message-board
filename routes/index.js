485c306e0e5747c178b7ea2c5f952cce25702524
Jared Mezquita <mezquitaj@gmail.com>
2021-06-16 22:12:03 -0400

    functioning express app

--- routes/index.js
+++ routes/index.js
@@ -18,16 +18,16 @@ const messages = [
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message board', messages: messages });
-  // res.next();
-});
-
-router.get('/new', function(req, res, next) {
-  res.render('form', {title: 'Enter Your Message'});
+  // res.send('testing');
 });
 
 router.post('/new', function(req, res, next) {
-  let auth = req.body;
-  // console.log(auth);
+  messages.push({
+    text: req.body.usertext, 
+    user: req.body.author, 
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
